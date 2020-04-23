23fc70439e1aedac71847da9fe119c2df0ae15d5
SoVa-web <54186792+SoVa-web@users.noreply.github.com>
2020-04-24 00:00:24 +0300

    init

---
+++ DS_IP96_LR6.2_SuprunO.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+class Graph{
+public:
+
+       void init(int lines, int stone, int** A){
+       A = new int*[lines];
+       for(int i = 0; i < lines; i++){
+              A[i] = new int[stones];
+       }
+       }
+private:
+       char* = {"list.txt"};
+       int versh, rebra;
+       int** listRibs;
+       int** adjMatrix;
+
+};
